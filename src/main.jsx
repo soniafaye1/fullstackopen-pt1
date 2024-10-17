162db70fc462d5b4fc20bc7c147c87b5fba89728
sonia <soniafaye1@gmail.com>
2024-10-16 20:03:09 -0400

    anecdotes 1 done

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App2 from './App2.jsx'
 import Unicafe from './Unicafe.jsx'
 import CourseInfo from './CourseInfo.jsx'
+import Anecdotes from './Anecdotes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         {/* <CourseInfo /> */}
-        <Unicafe/>
+        <Anecdotes />
+        {/* <Unicafe/> */}
     </>
 )
